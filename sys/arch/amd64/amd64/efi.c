4207fc15186901bb5acd3b7322bdfa3d973439e7
Sergii Dmytruk <sergii.dmytruk@3mdeb.com>
2022-08-09 14:08:02 +0300

    Start adding EFI ESRT driver

---
+++ sys/arch/amd64/amd64/efi.c
@@ -0,0 +1,82 @@
+#include <sys/param.h>
+#include <sys/device.h>
+#include <sys/systm.h>
+
+#include <uvm/uvm_extern.h>
+
+#include <machine/biosvar.h>
+#include <machine/cpufunc.h>
+#include <machine/bus.h>
+#include <machine/fpu.h>
+
+#include <dev/acpi/efi.h>
+
+#include <dev/clock_subr.h>
+
+struct efi_esrt {
+};
+
+int	efi_match(struct device *, void *, void *);
+void	efi_attach(struct device *, struct device *, void *);
+
+const struct cfattach efi_ca = {
+	sizeof(struct efi_esrt), efi_match, efi_attach
+};
+
+struct cfdriver efi_cd = {
+	NULL, "efi", DV_DULL
+};
+
+int
+efi_match(struct device *parent, void *match, void *aux)
+{
+	static EFI_GUID	esrt_guid = ESRT_TABLE_GUID;
+	EFI_SYSTEM_TABLE *st = (EFI_SYSTEM_TABLE *)PMAP_DIRECT_MAP(bios_efiinfo->system_table);
+	EFI_CONFIGURATION_TABLE *ct;
+	int i;
+
+	bus_space_tag_t		 iot = X86_BUS_SPACE_MEM;
+	bus_space_handle_t	 ioh;
+
+	if (bus_space_map(iot, bios_efiinfo->system_table, sizeof(*st),
+	    BUS_SPACE_MAP_PREFETCHABLE | BUS_SPACE_MAP_LINEAR, &ioh))
+		panic("can't map EFI_SYSTEM_TABLE");
+	st = bus_space_vaddr(iot, ioh);
+
+	if (bus_space_map(iot, (uintptr_t)st->ConfigurationTable, sizeof(*st) * st->NumberOfTableEntries,
+	    BUS_SPACE_MAP_PREFETCHABLE | BUS_SPACE_MAP_LINEAR, &ioh))
+		panic("can't map ConfigurationTable");
+	ct = bus_space_vaddr(iot, ioh);
+
+	printf(": ST %p\n", st);
+	printf(": ST# %lu\n", st->NumberOfTableEntries);
+
+	for (i = 0; i < st->NumberOfTableEntries; i++) {
+		if (efi_guidcmp(&esrt_guid, &ct[i].VendorGuid) == 0)
+			return 1;
+	}
+
+	printf(": Didn't find ESRT!\n");
+	return 0;
+}
+
+void
+efi_attach(struct device *parent, struct device *self, void *aux)
+{
+	uint64_t system_table;
+	EFI_SYSTEM_TABLE *st;
+	uint16_t major, minor;
+
+	system_table = bios_efiinfo->system_table;
+	KASSERT(system_table);
+
+	st = (void *)system_table;
+
+	major = st->Hdr.Revision >> 16;
+	minor = st->Hdr.Revision & 0xffff;
+	printf(": UEFI %d.%d", major, minor / 10);
+	if (minor % 10)
+		printf(".%d", minor % 10);
+	printf("\n");
+	return; // XXX
+}
